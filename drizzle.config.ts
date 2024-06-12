a894c061789cfff43da04c77e218be4ae9557df5
Arnold Chand <creativenull@outlook.com>
2024-06-12 14:05:39 +0000

    fix: dizzle config, use dialect

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,5 +3,5 @@ import { defineConfig } from "drizzle-kit";
 export default defineConfig({
   schema: "./server/database/schema.ts",
   out: "./server/database/migrations",
-  driver: "better-sqlite",
+  dialect: "sqlite",
 });
