50afce83f266ff1db943a12ef57fa1cbe449d534
Arnold Chand <creativenull@outlook.com>
2024-06-18 11:11:37 -0400

    fix: database import path

--- server/utils/useDatabase.ts
+++ server/utils/useDatabase.ts
@@ -1,5 +1,5 @@
 import { type BetterSQLite3Database, drizzle } from "drizzle-orm/better-sqlite3";
-import * as schema from "../database/schema";
+import * as schema from "~/database/schema";
 import Database from "better-sqlite3";
 
 export type AppDatabase = BetterSQLite3Database<typeof schema>;
