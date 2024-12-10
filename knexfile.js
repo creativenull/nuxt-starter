cb43659adc26f6513239f70863c85fba46017783
Arnold Chand <creativenull@outlook.com>
2024-12-10 00:07:13 -0400

    feat: init knex setup

---
+++ knexfile.js
@@ -0,0 +1,17 @@
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+const config = {
+  development: {
+    client: "better-sqlite3",
+    useNullAsDefault: true,
+    connection: {
+      filename: "./storage/db.sqlite",
+    },
+    migrations: {
+      directory: "./src/server/database/knex/migrations"
+    }
+  },
+};
+
+export default config;
---
+++ src/server/database/knex/migrations/20241210035531_users_table.js
@@ -0,0 +1,22 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export const up = function (knex) {
+  return knex.schema.createTable("users", (t) => {
+    t.increments();
+    t.string("first_name", 255).notNullable();
+    t.string("last_name", 255).notNullable();
+    t.string("email", 255).notNullable().unique();
+    t.string("password", 128).notNullable();
+    t.timestamps();
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export const down = function (knex) {
+  return knex.schema.dropTable("users");
+};
