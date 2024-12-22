c27be1bab52d0d61ebee8bd7117dabd3ca39d223
Arnold Chand <creativenull@outlook.com>
2024-12-22 15:50:50 -0400

    fix: use in a container

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,11 +4,11 @@ const { loggedIn, clear } = useUserSession();
 </script>
 
 <template>
-  <div v-if="!loggedIn" class="space-x-2">
-    <UButton color="primary" variant="solid" to="/login"> Login </UButton>
-    <UButton color="primary" variant="solid" to="/register"> Register </UButton>
-  </div>
-  <div v-else class="space-x-2">
-    <UButton color="primary" variant="solid" @click="clear"> Logout </UButton>
-  </div>
+  <UContainer>
+    <div class="space-x-2">
+      <UButton v-if="!loggedIn" color="primary" variant="solid" to="/login"> Login </UButton>
+      <UButton v-if="!loggedIn" color="primary" variant="solid" to="/register"> Register </UButton>
+      <UButton v-if="loggedIn" color="primary" variant="solid" @click="clear"> Logout </UButton>
+    </div>
+  </UContainer>
 </template>
