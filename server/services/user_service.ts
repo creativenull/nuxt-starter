761028f4c804141324cc748b18308c0651c480e1
Arnold Chand <creativenull@outlook.com>
2026-02-16 09:24:54 -0400

    feat: user service

---
+++ server/services/user_service.ts
@@ -0,0 +1,52 @@
+import bcrypt from "bcrypt";
+import "dotenv/config";
+import { eq } from "drizzle-orm";
+import { useDatabase } from "~~/database";
+import { usersTable } from "~~/database/schema";
+
+export async function getUser(id: number) {
+  const db = useDatabase();
+
+  const result = await db
+    .select({ id: usersTable.id, name: usersTable.name, email: usersTable.email })
+    .from(usersTable)
+    .where(eq(usersTable.id, id));
+
+  return result && result[0] ? result[0] : null;
+}
+
+export async function getUserByEmail(email: string) {
+  const db = useDatabase();
+
+  const result = await db
+    .select({ id: usersTable.id, name: usersTable.name, email: usersTable.email })
+    .from(usersTable)
+    .where(eq(usersTable.email, email));
+
+  return result && result[0] ? result[0] : null;
+}
+
+export async function setUser(name: string, email: string, plainTextPassword: string) {
+  const db = useDatabase();
+  const saltRounds = parseInt(process.env.NUXT_BCRYPT_SALT_ROUNDS ?? "12", 10);
+  const salt = await bcrypt.genSalt(saltRounds);
+  const password = await bcrypt.hash(plainTextPassword, salt);
+
+  const user = await db
+    .insert(usersTable)
+    .values({ name, email, password })
+    .returning({ id: usersTable.id, name: usersTable.name, email: usersTable.email });
+
+  return user;
+}
+
+export async function deleteUser(id: number) {
+  const db = useDatabase();
+
+  const result = await db
+    .delete(usersTable)
+    .where(eq(usersTable.id, id))
+    .returning({ id: usersTable.id });
+
+  return result && result[0] ? result[0] : null;
+}
